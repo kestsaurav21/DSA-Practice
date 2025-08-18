99cce3af183792feac0471b2f2d49503f43c95a5
kestsaurav21 <kestsaurav21@outlook.com>
2025-08-18 15:25:52 +0530

    optimized solution

--- Array/Sliding Window/sumOfSubarray.py
+++ Array/Sliding Window/sumOfSubarray.py
@@ -15,3 +15,21 @@ def sumOfSubArray(arr, k):
 arr = [4,7,3,1,5,2]
 k = 3
 print(sumOfSubArray(arr, k))
+
+# Optimized - using sliding window
+
+def subArrayOfK(arr, k):
+    n = len(arr)
+    sum=0
+    for i in range(k):
+        sum+=arr[i]
+
+    for j in range(k,n):
+        print(sum)
+        sum+=arr[j]
+        sum-=arr[j-k]
+    print(sum)
+
+arr = [4,7,3,1,5,2]
+k = 3
+subArrayOfK(arr, k)
