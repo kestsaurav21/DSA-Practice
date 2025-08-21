c4b292a523e247f5b16e49c25b0811f9fb7fa69c
kestsaurav21 <kestsaurav21@outlook.com>
2025-08-21 14:31:42 +0530

    two sum

---
+++ Array/Two Pointer/TwoSum.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+
+        number = {}  # dictionary - hash map to store the number and their indices
+
+        for i, num in enumerate(nums):
+            if target - num in number:
+                return [i, number[target-num]]
+
+            number[num] = i
+
+        return -1
