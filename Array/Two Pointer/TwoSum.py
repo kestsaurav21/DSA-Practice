c888edc2ab7130025494032e6eca466d6002b645
kestsaurav21 <kestsaurav21@outlook.com>
2025-08-21 14:33:18 +0530

    two sum updated

--- Array/Two Pointer/TwoSum.py
+++ Array/Two Pointer/TwoSum.py
@@ -1,3 +1,18 @@
+
+# Brute Forec
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        for i in range(n):
+            for j in range(i+1, n):
+                if (nums[i] + nums[j] == target):
+                    return [i, j]
+
+
+# Optimized
+
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         n = len(nums)
