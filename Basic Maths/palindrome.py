ce3acf626e4d67b30abb635e392741ba70251a06
kestsaurav21 <kestsaurav21@outlook.com>
2025-09-01 16:48:40 +0530

    recursive method

--- Basic Maths/palindrome.py
+++ Basic Maths/palindrome.py
@@ -13,20 +13,37 @@
 #     return "Not a Palindrome"
 
 
-def palindrome(num):
-    num = str(num)
-    left = 0
-    right= len(num)-1
+# def palindrome(num):
+#     num = str(num)
+#     left = 0
+#     right= len(num)-1
 
-    while left<right:
-        if num[left] == num[right]:
-            left+=1
-            right-=1
-        else: 
-            return "Not a palindrome"
+#     while left<right:
+#         if num[left] == num[right]:
+#             left+=1
+#             right-=1
+#         else: 
+#             return "Not a palindrome"
 
-    return "Palindrome"
+#     return "Palindrome"
 
 
-num = int(input("Enter the number: "))
-print(palindrome(num))
+# num = int(input("Enter the number: "))
+# print(palindrome(num))
+
+# recursive method
+def checkPalindrome(s, i):
+
+    n = len(s)
+    if i >= n//2:
+        return "String is palindrome"
+    
+    if s[i] != s[n-i-1]:
+        return "String is not palindrome"
+
+    return checkPalindrome(s, i+1)
+
+
+if __name__ == "__main__":  
+    s = str(input("Enter the string: "))
+    print(checkPalindrome(s, 0))
